2a8be596efbbdc887347f463c7870449a7edd786
yeby <1096129283@qq.com>
2019-05-05 16:39:32 +0800

    2

---
+++ api.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * User: yeby
+ * Date: 2019/5/5
+ * Time: 16:34
+ */
+
+return GET["echostr"];
+//echostr
+//private function checkSignature()
+//{
+//    _GET["signature"];
+//    _GET["timestamp"];
+//    _GET["nonce"];
+//
+//    tmpArr = array(timestamp, $nonce);
+//    sort($tmpArr, SORT_STRING);
+//    $tmpStr = implode( $tmpArr );
+//    $tmpStr = sha1( $tmpStr );
+//
+//    if( signature ){
+//        return true;
+//    }else{
+//        return false;
+//    }
+//}
